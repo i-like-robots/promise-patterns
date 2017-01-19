8aacdbbf3ec801a6de5379e61abf56b0f982a849
Matt Hinchliffe <matt.hinchliffe@ft.com>
2017-01-19 14:38:45 +0000

    Add zip method that acts like Promise.all() but for objects

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   chunk: require('./lib/chunk'),
   retry: require('./lib/retry'),
   series: require('./lib/series'),
-  waterfall: require('./lib/waterfall')
+  waterfall: require('./lib/waterfall'),
+  zip: require('./lib/zip'),
 }
